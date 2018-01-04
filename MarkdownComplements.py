e152939d4296bb4cdf8e46f5f0278413c5804772
jun.shimoji <jun.shimoji@mobilus.co.jp>
2018-01-05 03:20:37 +0900

    * Changed repo name to match guidelines (PascalCase). * Quit while loop to self.view.line(pos[0]).begin or something like that.

--- MarkdownComplements.py
+++ MarkdownComplements.py
@@ -6,62 +6,32 @@ class MarkdownIndentDownCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         # print("\n-------MarkdownIndentDownCommand--------")
         pos = self.view.sel()
-        cursor = pos[0].a
-        # cursor is a number which represents the location info.
+        ## pre_cursor is a number which represents the previous "\n" location.
+        pre_cursor = self.view.line(pos[0]).begin()-1
+        # print("pre_cursor: ",pre_cursor)
+        ## cursor is a number which represents the end of the line location.
+        cursor = self.view.line(pos[0].a).end()
         # print("cursor: ",cursor)
-        # investigating the location of previous "\n"
-        i = 1
-        pre_cursor          = cursor
         previous_line_str   = ''
         previous_header     = ''
-        while(True):
-            pre_cursor -= 1
-            previousChar = self.view.substr(pos[0].a-i)
-            if(previousChar == '\n' or pre_cursor < 0):
-                break
-            i += 1
-        # print("previous cursor's location: ",pre_cursor)
         if(pre_cursor >= 0):
-            previous_line_str     = self.view.substr(self.view.line(pos[0].a-i))
+            previous_line_str     = self.view.substr(self.view.line(pre_cursor))
             previous_header       = re.search('^ *[\+|\-|*]',previous_line_str)
             if(previous_header):
                 previous_indent_level = previous_header.end()
                 # print("previous_indent_level: ",previous_indent_level)
         # print("previous_header: ",previous_header)
         # print("previous_line_str: ",previous_line_str)
-        # print("char num to previous enter(i): ",i)
-
-        # current line's investigation
+        ## current line's investigation
         current_line_str                = self.view.substr(self.view.line(pos[0].a))
         current_header                  = re.search('^ *[\+|\-|*]',current_line_str)
-        # if current line has any header(* or + or -)
+        ## if current line has any header(* or + or -)
         if(current_header):
             current_indent_level    = current_header.end()
             current_header_str      = current_line_str[:current_indent_level]
             # print("indent level of current line: ",current_indent_level)
             # print("location of current line's header: ",current_header.end())
             # print("header type of current line: ",current_header_str)
-
-        # following line's investigation
-        j = 0
-        following_cursor    = cursor
-        selfViewSize        = self.view.size()
-        selfViewLine        = self.view.line(selfViewSize)
-        # print("selfViewLine: ",selfViewLine.a)
-        # print("cursor : ",cursor)
-        # selfViewLine.a represents last line's location
-        if(cursor < selfViewLine.a):
-            while(True):
-                following_cursor += 1
-                afterChar = self.view.substr(pos[0].a+j)
-                # print("afterChar: ",afterChar)
-                if(afterChar == '\n' or following_cursor == 1000):
-                    break
-                j += 1
-        # print("current_header: ",current_header)
-        # print("current_line_str: ",current_line_str)
-        # print("char num to next enter(j): ",j)
-
         ## rule1:  Current line has no header. And previous line has header.
         ## action: Input previous line's same header at indent level.
         if(not current_header) and (previous_header):
@@ -69,59 +39,45 @@ class MarkdownIndentDownCommand(sublime_plugin.TextCommand):
             # print("lstriped current_line_str: ",current_line_str.strip())
             change_line = previous_line_str[:previous_indent_level] + " "+current_line_str.strip()
             # print("change_line: ",change_line)
-            region = sublime.Region(pos[0].a-i+1, pos[0].a)
+            region = sublime.Region(pre_cursor+1, pos[0].a)
+            ## Don't use "replace" because the area is selected.
             self.view.erase(edit,region)
             self.view.insert(edit,pos[0].a,change_line)
-            # Don't use "replace" because the area is highlighted.
-            # region = sublime.Region(pos[0].a-i+1, pos[0].a+j)
-            # self.view.replace(edit,region,change_line)
-
         ## rule2: Previous line and current line have a header.
         ## action: Indent down.
         elif (current_header):
             # print("rule2")
             change_line = "\t"
-            self.view.insert(edit,pos[0].a-i+1,change_line)
+            # self.view.insert(edit,pos[0].a-i+1,change_line)
+            self.view.insert(edit,pre_cursor+1,change_line)
         ## rule3: Previous line and current line have no header.
         ## action: Making a header(*)
         elif (not current_header) and (not previous_header):
             # print("rule3")
-            print("lstriped current_line_str: ",current_line_str.strip())
-            region = sublime.Region(pos[0].a-i+1, pos[0].a+j)
+            # print("lstriped current_line_str: ",current_line_str.strip())
+            region = sublime.Region(pre_cursor+1,cursor)
+            # Don't use "replace" because the area is selected.
             self.view.erase(edit,region)
             self.view.insert(edit,pos[0].a,"* "+current_line_str.strip())
-            # Don't use "replace" because the area is highlighted.
-            # self.view.replace(edit,region,"* "+current_line_str.strip())
 
 class MarkdownIndentUpCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         # print("\n-------MarkdownIndentUpCommand--------")
         myTabSize = self.view.settings().get('tab_size')
         pos                 = self.view.sel()
-        cursor              = pos[0].a
-
-        # investigating the location of previous "\n"
-        i = 1
-        pre_cursor = cursor
-        while(True):
-            pre_cursor -= 1
-            previousChar = self.view.substr(pos[0].a-i)
-            if(previousChar == '\n' or pre_cursor < 0):
-                break
-            i += 1
+        # investigating the begining location of current line
+        bgn_cursor = self.view.line(pos[0]).begin()
         current_line_str    = self.view.substr(self.view.line(pos[0].a))
         current_header      = re.search('^ *[\+|\-|*]',current_line_str)
         # print("current_line_str: ",current_line_str)
-        # print("char num to previous enter: ",i)
         ## if current line has any header(* or + or -)
         if(current_header):
             current_indent_level = current_header.end()
             current_header_str      = current_line_str[:current_indent_level]
             # print("indent level of current line: ",current_indent_level)
             # print("header type of current line: ",current_header_str)
-
             if(current_indent_level > myTabSize):
-                region = sublime.Region(pos[0].a-i+1, pos[0].a-i+1+myTabSize)
+                region = sublime.Region(bgn_cursor, bgn_cursor+myTabSize)
                 self.view.erase(edit,region)
 
 class MarkdownNewLineCommand(sublime_plugin.TextCommand):
@@ -142,26 +98,15 @@ class MarkdownNewLineCommand(sublime_plugin.TextCommand):
 class MarkdownRotateListItem(sublime_plugin.TextCommand):
     def run(self, edit):
         # print("\n-------MarkdownRotateHeaderCommand--------")
-        myTabSize = self.view.settings().get('tab_size')
         pos                 = self.view.sel()
-        cursor              = pos[0].a
-
-        ## investigating the location of previous "\n"
-        i = 1
-        pre_cursor = cursor
-        while(True):
-            pre_cursor -= 1
-            previousChar = self.view.substr(pos[0].a-i)
-            if(previousChar == '\n' or pre_cursor < 0):
-                break
-            i += 1
+        ## investigating the begining location of current line
+        bgn_cursor = self.view.line(pos[0]).begin()
         current_line_str        = self.view.substr(self.view.line(pos[0].a))
         current_header          = re.search('^ *[\+|\-|*]',current_line_str)
         if(current_header):
             current_indent_level    = current_header.end()
             current_header_str      = current_line_str[:current_indent_level]
             # print("indent level of current line: ",current_indent_level)
-            # print("location of current line's header: ",current_header.end())
             # print("header type of current line:",current_header_str)
             if(current_header_str.find("*") >= 0):
                 changed_header = current_line_str[0:current_header.end()].replace("*","+")
@@ -170,5 +115,5 @@ class MarkdownRotateListItem(sublime_plugin.TextCommand):
             elif(current_header_str.find("-") >= 0):
                 changed_header = current_line_str[0:current_header.end()].replace("-","*")
             # print("changed_header: ",changed_header)
-            region = sublime.Region(pos[0].a-i+1, pos[0].a-i+1+current_indent_level+1)
-            self.view.replace(edit,region,changed_header+" ")
+            region = sublime.Region(bgn_cursor, bgn_cursor+current_indent_level)
+            self.view.replace(edit,region,changed_header)
